e37aec2cb3b7e42aa53a0a7f6341a592e016ea10
Kris van Rens <krisvanrens@gmail.com>
2022-07-15 23:40:07 +0200

    ADD: First exercise.

---
+++ source/exercise01_coroutine_traits.cpp
@@ -0,0 +1,22 @@
+#include <exception>
+#include <future>
+#include <iostream>
+
+std::future<int> foo() {
+  // std::promise<int> p;
+  // auto f = p.get_future();
+  // try {
+  //   int i = 42;
+  //   p.set_value(i);
+  // }
+  // catch(...) {
+  //   p.set_exception(std::current_exception());
+  // }
+  // return f;
+
+  co_return 42;
+}
+
+int main() {
+  std::cout << foo().get() << "\n";
+}
